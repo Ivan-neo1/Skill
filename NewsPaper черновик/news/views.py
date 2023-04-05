041f23e253520a44975718438fadc0089a622218
Ivan <ryumkin53@gmail.com>
2023-04-05 17:35:45 +0300

    chernovik

---
+++ NewsPaper черновик/news/views.py
@@ -0,0 +1,117 @@
+from django.views.generic import ListView, DetailView, CreateView,\
+    UpdateView, DeleteView, TemplateView
+from django.urls import reverse_lazy
+from .filters import PostFilter
+from .models import Post, Category, CategorySubscribe, User
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, get_object_or_404, render
+from django.contrib.auth.models import Group
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views import View
+from django.core.mail import EmailMultiAlternatives
+from datetime import datetime
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+
+class ProductsList(ListView):
+    model = Post
+    ordering = 'author'
+    template_name = 'news.html'
+    context_object_name = 'post'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+class News_id(DetailView):
+    model = Post
+    ordering = '-creation_time'
+    template_name = 'news_id.html'
+    context_object_name = 'post_id'
+
+class PostCreate(CreateView, LoginRequiredMixin, PermissionRequiredMixin):
+    form_class = PostForm
+    model = Post
+    template_name = 'create_news.html'
+    permission_required = ('news.add_post',)
+
+    def form_valid(self, form):
+        news = form.save(commit=False)
+        news.article_or_news = 'NWS'
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_author'] = not self.request.user.groups.filter(name='authors').exists()
+        return context
+
+class ArticleCreate(CreateView, LoginRequiredMixin, PermissionRequiredMixin):
+    form_class = PostForm
+    model = Post
+    template_name = 'create_article.html'
+    permission_required = ('article.add_post',)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_author'] = not self.request.user.groups.filter(name='authors').exists()
+        return context
+
+    def form_valid(self, form):
+        article = form.save(commit=False)
+        article.article_or_news = 'ART'
+        return super().form_valid(form)
+#При создании поста после авторизации будет появляться кнопка "Хочу стать автором"
+@login_required
+def upgrade_me(request):
+    user = request.user
+    authors_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        authors_group.user_set.add(user)
+    return redirect('/news/')
+
+class NewsEdit(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+    permission_required = ('news.change_post',)
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('post_list')
+
+class ArticleEdit(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'article_edit.html'
+    permission_required = ('article.change_post',)
+
+
+
+
+class CategoriesView(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'categories.html', {'categories': Category.objects.all()})
+
+class CategoryDetail(LoginRequiredMixin, DetailView):
+
+    model = Category
+    template_name = 'category.html'
+    context_object_name = 'category'
+
+
+def subscribe_to_category(request, pk):
+
+    current_user = request.user
+    CategorySubscribe.objects.create(category=Category.objects.get(pk=pk), subscriber=User.objects.get(pk=current_user.id))
+
+    return render(request, 'subscribed.html')

183120ce61b83f2d493c7a11272fc743930ba48b
Ivan <ryumkin53@gmail.com>
2023-04-06 19:12:55 +0300

    t Please enter the commit message for your changes. Lines starting

---
+++ Черновик 06.04.23/news/templatetags/custom_filters.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+censored_words = ['редиска']
+
+@register.filter()
+def censor(value):
+    if not isinstance(value, str):
+        raise ValueError("фильтр только для строк")
+
+    words = value.split()
+    for i, word in enumerate(words):
+        if word.lower() in censored_words:
+            words[i] = '*' * len(word)
+    return ' '.join(words)
